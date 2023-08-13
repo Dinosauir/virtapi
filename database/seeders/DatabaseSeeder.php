82f5d0726dd749e23d6308d38a03ee32cf605049
Sebastian <sebastianvlasceanu@icloud.com>
2023-08-13 16:51:49 +0300

    added workflows

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        Station::factory(50000)->create();
+        Station::factory(5)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
